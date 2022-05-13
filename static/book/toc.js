96eb2fb4a9583cff058752063162545a34f0a469
Martin Desruisseaux <martin.desruisseaux@geomatys.com>
2022-05-13 18:56:16 +0200

    Table Of Content (TOC) highlights the viewed item in the main document.

---
+++ static/book/toc.js
@@ -0,0 +1,31 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF)
+ * under one or more contributor license agreements.
+ */
+window.addEventListener('DOMContentLoaded', () => {
+
+  var lastVisibleId = null;
+
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      const id = entry.target.getAttribute('id');
+      if (entry.intersectionRatio > 0) {
+        if (lastVisibleId !== null) {
+            document.querySelector(`nav li a[href="#${lastVisibleId}"]`).parentElement.classList.remove('active');
+        }
+        lastVisibleId = id;
+        var item = document.querySelector(`nav li a[href="#${id}"]`).parentElement;
+        item.classList.add('active');
+        const bounds = item.getBoundingClientRect();
+        if (bounds.top < 0 || bounds.bottom > window.innerHeight - 2) {
+            item.scrollIntoView();
+        }
+      }
+    });
+  });
+
+  // Track headers that have an `id` applied.
+  document.querySelectorAll(':is(h1,h2,h3,h4)[id]').forEach((section) => {
+    observer.observe(section);
+  });
+});
