daec927f6884dd8536d09924b7e0df17b0c1389e
Martin Desruisseaux <martin.desruisseaux@geomatys.com>
2022-05-19 19:37:30 +0200

    Reorganise the developer guide with more text moved to annex.

--- static/book/toc.js
+++ static/book/toc.js
@@ -9,16 +9,19 @@ window.addEventListener('DOMContentLoaded', () => {
   const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
       const id = entry.target.getAttribute('id');
-      if (entry.intersectionRatio > 0) {
-        if (lastVisibleId !== null) {
-            document.querySelector(`nav li a[href="#${lastVisibleId}"]`).parentElement.classList.remove('active');
-        }
-        lastVisibleId = id;
-        var item = document.querySelector(`nav li a[href="#${id}"]`).parentElement;
-        item.classList.add('active');
-        const bounds = item.getBoundingClientRect();
-        if (bounds.top < 0 || bounds.bottom > window.innerHeight - 2) {
-            item.scrollIntoView();
+      var line = document.querySelector(`nav li a[href="#${id}"]`);
+      if (line !== null) {
+        if (entry.intersectionRatio > 0) {
+          if (lastVisibleId !== null) {
+              document.querySelector(`nav li a[href="#${lastVisibleId}"]`).parentElement.classList.remove('active');
+          }
+          lastVisibleId = id;
+          var item = line.parentElement;
+          item.classList.add('active');
+          const bounds = item.getBoundingClientRect();
+          if (bounds.top < 0 || bounds.bottom > window.innerHeight - 2) {
+              item.scrollIntoView();
+          }
         }
       }
     });
